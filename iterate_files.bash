4e0bdcfacb140df303bb8cd95269546aa46c44df
Amit Einav <38571744+amiteinav@users.noreply.github.com>
2019-03-03 11:03:58 +0200

    Update iterate_files.bash

--- iterate_files.bash
+++ iterate_files.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `gsutil ls -p fnx-speech2text gs://bucket-it-speech2text-poc-1/ | grep -i ".wav"`
+for i in `gsutil ls -p name-speech2text gs://bucket-it-speech2text-poc-1/ | grep -i ".wav"`
 do
    : 
    ls $i
