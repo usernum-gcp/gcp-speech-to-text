e04b34b71649d489a79e5736a569bccf42ec4eb0
Amit Einav <38571744+amiteinav@users.noreply.github.com>
2019-03-03 11:00:50 +0200

    Update iterate_gcs_to_speech.bash

--- iterate_gcs_to_speech.bash
+++ iterate_gcs_to_speech.bash
@@ -4,6 +4,6 @@ for i in `gsutil ls -r gs://fnx-it-* | grep .wav`
 do
    : 
    echo $i
-   python /home/amiteinav_google_com/gcp-speech-to-text/extract_fnx_input_to_csv.py $i &
+   python /home/amiteinav_google_com/gcp-speech-to-text/extract_NICE_input_to_csv.py $i &
    sleep 15
 done
