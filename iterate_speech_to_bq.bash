45219f56758ab46df07f36fd136eb69ca57991bc
Amit Einav <38571744+amiteinav@users.noreply.github.com>
2019-03-03 11:02:21 +0200

    Update iterate_speech_to_bq.bash

--- iterate_speech_to_bq.bash
+++ iterate_speech_to_bq.bash
@@ -4,11 +4,9 @@ file=/tmp/tmpfile$$
 
 cat *.csv | grep -v "call_id,word,start_time,end_time,confidence,sentence" > ${file}
 
-bq load --source_format=CSV   fnx-speech2text:fnx_speech_to_text_poc.fnx_speech_to_text_poc_facts ${file} call_id,word,start_time:float,end_time:float,confidence:float,sentence:string
+bq load --source_format=CSV   name-speech2text:name_speech_to_text_poc.name_speech_to_text_poc_facts ${file} call_id,word,start_time:float,end_time:float,confidence:float,sentence:string
 
 rm ${file}
-
-#bq load --source_format=CSV --skip_leading_rows=1  fnx-speech2text:fnx_speech_to_text_poc.fnx_speech_to_text_poc_facts ${file} call_id,word,start_time:float,end_time:float,confidence:float,sentence:string
 
 exit
 
@@ -16,6 +14,6 @@ for i in `ls -1 * | grep ".csv"`
 do
    : 
    echo $i
-	bq load --source_format=CSV --skip_leading_rows=1  fnx-speech2text:fnx_speech_to_text_poc.fnx_speech_to_text_poc_facts ${i} call_id,word,start_time:float,end_time:float,confidence:float  
+	bq load --source_format=CSV --skip_leading_rows=1  name-speech2text:name_speech_to_text_poc.name_speech_to_text_poc_facts ${i} call_id,word,start_time:float,end_time:float,confidence:float  
  
 done
