f8bd85e6289660aa9a2ddd42f7bea8ca0839c6dc
Amit Einav <amiteinav@google.com>
2019-04-02 01:10:54 +0300

    Create get_wav_length.py

---
+++ get_wav_length.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import time, argparse, datetime, csv,sys,getopt
+
+'''
+	-f <file-name> # which file to check
+	-o <output-file-name> # send output to a tsv file: filename\tlength-in-seconds
+
+'''
+
+def get_wav_length(filename):
+	import wave
+	import contextlib
+	fname = filename
+	with contextlib.closing(wave.open(fname,'r')) as f:
+		frames = f.getnframes()
+		rate = f.getframerate()
+		return ( frames / float(rate) )
+
+def main(argv):
+
+	got_output_file = False
+	try:
+		opts, args = getopt.getopt(argv,"f:o:")
+	except getopt.GetoptError:
+		sys.exit(42)
+
+	for opt, arg in opts:
+		if opt == "-f":
+			filepath = arg
+			is_local = True
+		elif opt == "-o": 
+			output_file = arg
+			got_output_file = True
+
+	string = filepath + '\t' + str(get_wav_length(filepath))
+
+	if (got_output_file):
+		with open(output_file, "a") as myfile:
+			myfile.write(string)
+			myfile.close()
+
+	print string
+
+
+
+if __name__ == "__main__":
+    script_name=sys.argv[0]
+    arguments = len(sys.argv) - 1
+
+    if (arguments == 0):
+        sys.exit(42)
+
+    main(sys.argv[1:])
+
