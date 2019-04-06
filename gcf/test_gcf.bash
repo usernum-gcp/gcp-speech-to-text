bf748f509a3bd6bd064c3d7341ccfb81e9382c53
Amit Einav <amiteinav@google.com>
2019-04-06 22:59:43 +0300

    Update test_gcf.bash

--- gcf/test_gcf.bash
+++ gcf/test_gcf.bash
@@ -1,26 +1,22 @@
 #!/bin/bash
 
-samplefile=0502112239_1_6397392588207366206_1_32.wav
+source params.bash
 
 echo "Using file: $samplefile"
 
-if [ "$1" == "" ] ; then
-	exit 1
-else
+if [ "$1" != "" ] ; then
 	BUCKET=$1
 fi
 
 echo "bash deploy_gcf.bash ${BUCKET}"
-
 bash deploy_gcf.bash $BUCKET
 
 echo "gsutil cp $samplefile gs://${BUCKET}/wav/legal-services/"
-
 gsutil cp $samplefile gs://${BUCKET}/wav/legal-services/
 
-echo "sleep 12"
-sleep 12
+echo "sleep 15"
+sleep 15
 
 #https://cloud.google.com/sdk/gcloud/reference/beta/functions/logs/read
-echo "gcloud beta functions logs read gcf_speech_to_text --limit 1000"
-gcloud beta functions logs read gcf_speech_to_text --limit 1000
+echo "gcloud beta functions logs read ${TRANSCRIBE_FUNCTION_NAME} --limit 50"
+gcloud beta functions logs read ${TRANSCRIBE_FUNCTION_NAME} --limit 50
