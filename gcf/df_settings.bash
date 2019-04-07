543d91bc4f7810fb2959f9246ebf0369728d5148
Amit Einav <amiteinav@google.com>
2019-04-07 14:53:53 +0300

    Delete df_settings.bash

--- gcf/df_settings.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-gcloud dataflow jobs run JOB_NAME \
-    --gcs-location gs://dataflow-templates/latest/PubSub_to_BigQuery \
-    --parameters \
-inputTopic=projects/YOUR_PROJECT_ID/topics/YOUR_TOPIC_NAME,\
-outputTableSpec=YOUR_PROJECT_ID:YOUR_DATASET.YOUR_TABLE_NAME
