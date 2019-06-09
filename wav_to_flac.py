c07fb169f86d0f312a260d438e65d05b28b95595
Amit Einav <amiteinav@google.com>
2019-06-09 19:56:17 +0300

    Create wav_to_flac.py

---
+++ wav_to_flac.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from os.path import splitext
+from pydub import AudioSegment
+
+def wav2flac(wav_path):
+    flac_path = "%s.flac" % splitext(wav_path)[0]
+    song = AudioSegment.from_wav(wav_path)
+    song.export(flac_path, format = "flac")
+
+if __name__ == "__main__":
+    import sys
+    wav2flac(sys.argv[1])
