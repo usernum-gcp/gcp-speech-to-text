a8f7db3a8458b240ce5b20a040dced762fd71a20
Amit Einav <amiteinav@google.com>
2019-06-10 09:19:49 +0300

    Update iterate_files.bash

--- iterate_files.bash
+++ iterate_files.bash
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # bash iterate_files.bash GS_PATH FTYPE LANG
-# bash iterate_files.bash gs://example/speech flac he-IL
+# bash iterate_files.bash gs://example/speech flac he-IL DST_FOLDER
 
 GS_PATH=$1
 FTYPE=$2
 LANG=$3
+DST_FOLDER=$4
 
 TRANSCRIBE_PY=${HOME}/gcp-speech-to-text/transcribe_word_time_offsets.py
+
+cd ${DST_FOLDER}
 
 for i in `gsutil ls -r $GS_PATH | grep -i ${FTYPE}`
 do
