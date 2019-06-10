d425173e0698ec219ba2390add61cc4c289ef1b2
Amit Einav <amiteinav@google.com>
2019-06-10 10:51:27 +0300

    Update iterate_load_stats_to_bq.bash

--- iterate_load_stats_to_bq.bash
+++ iterate_load_stats_to_bq.bash
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-PROJECT_ID=name-speech2text
-DATASET=name_speech_to_text_poc
-TABLE=name_speech_to_text_poc_loading_stats
-FOLDER="~/20190101/upload_stats/"
+PROJECT_ID=$1
+DATASET=$2
+LOCAL_FOLDER=$3
+TABLE=speech_to_text_loading_stats
 
-if [ "$#" -ne "0" ] ; then
-	FOLDER=${1}	
-fi
+# bash iterate_load_stats_to_bq.bash ${PROJECT_ID} ${DATASET} ${LOCAL_FOLDER} 
 
 bq rm -f -t ${PROJECT_ID}:${DATASET}.${TABLE}
 
 file=/tmp/tmpfile$$
 
-cat ${FOLDER}*.csv | grep -v "gcs_uri,call_id,processing_elapsed_seconds,call_length,csv_file_name" > ${file}
+cat ${LOCAL_FOLDER}/meta_*.csv | grep -v "gcs_uri,call_id,processing_elapsed_seconds,call_length,csv_file_name" > ${file}
 
 bq load --source_format=CSV  ${PROJECT_ID}:${DATASET}.${TABLE} ${file} gcs_uri,call_id:integer,processing_elapsed_seconds:float,call_length:float,csv_file_name
 
