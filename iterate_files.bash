dc764787703238eacdff32524608934a7c7f56b8
Amit Einav <amiteinav@google.com>
2019-06-10 09:17:12 +0300

    Update iterate_files.bash

--- iterate_files.bash
+++ iterate_files.bash
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-for i in `gsutil ls -p name-speech2text gs://bucket-it-speech2text-poc-1/ | grep -i ".wav"`
+# bash iterate_files.bash GS_PATH FTYPE LANG
+# bash iterate_files.bash gs://example/speech flac he-IL
+
+GS_PATH=$1
+FTYPE=$2
+LANG=$3
+
+TRANSCRIBE_PY=${HOME}/gcp-speech-to-text/transcribe_word_time_offsets.py
+
+for i in `gsutil ls -r $GS_PATH | grep -i ${FTYPE}`
 do
    : 
-   ls $i
+   echo "Now Handling $i"
+   echo "python ${TRANSCRIBE_PY} -u $i -l '${LANG}' -i -e $2  "
+
 done
